22fa2f7ae09c212cb80397908f0e886db56c9923
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-02-08 07:39:31 -0500

    fix(products): fix product table

--- src/views/dashboard/Products/index.js
+++ src/views/dashboard/Products/index.js
@@ -1,9 +1,30 @@
-import { Typography } from '@mui/material';
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Box, width } from '@mui/system';
+import ProductsList from './ProductsList';
 
-export default function Products() {
+export default function Projects() {
   return (
-    <section>
-      <Typography sx={{ fontSize: '3rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>Products Report</Typography>
-    </section>
+    <Grid container direction="column">
+      <Grid item>
+        <Box sx={{ width: '100%' }}>
+          <Paper sx={{ mb: 2, p: 2 }}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Typography sx={{ fontSize: '2rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>All Products</Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" endIcon={<AddIcon />}>
+                  Add New Product
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Box>
+      </Grid>
+      <Grid item>
+        <ProductsList />
+      </Grid>
+    </Grid>
   );
 }
