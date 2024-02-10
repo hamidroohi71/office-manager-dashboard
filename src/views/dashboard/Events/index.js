31fb6baa1d5a5b59ce13d78872984261a4737c48
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-02-10 03:59:09 -0500

    fix(events page): add needed components

--- src/views/dashboard/Events/index.js
+++ src/views/dashboard/Events/index.js
@@ -1,9 +1,30 @@
-import { Typography } from '@mui/material';
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Box, width } from '@mui/system';
+import EventsList from './EventsList';
 
 export default function Events() {
   return (
-    <section>
-      <Typography sx={{ fontSize: '3rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>Events Report</Typography>
-    </section>
+    <Grid container direction="column">
+      <Grid item>
+        <Box sx={{ width: '100%' }}>
+          <Paper sx={{ mb: 2, p: 2 }}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Typography sx={{ fontSize: '2rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>All Events</Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" endIcon={<AddIcon />}>
+                  Add New Event
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Box>
+      </Grid>
+      <Grid item>
+        <EventsList />
+      </Grid>
+    </Grid>
   );
 }
