ca9cdbcbdd506eae007961259d06e4f3f0c5cf93
Hamid Roohi <52012062+hamidroohi71@users.noreply.github.com>
2024-02-14 05:48:28 -0500

    fix(marketing): fix marketing page

--- src/views/dashboard/Marketing/index.js
+++ src/views/dashboard/Marketing/index.js
@@ -1,9 +1,30 @@
-import { Typography } from '@mui/material';
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Box, width } from '@mui/system';
+import ClientsList from './ClientsList';
 
 export default function Marketing() {
   return (
-    <section>
-      <Typography sx={{ fontSize: '3rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>Marketing Report</Typography>
-    </section>
+    <Grid container direction="column">
+      <Grid item>
+        <Box sx={{ width: '100%' }}>
+          <Paper sx={{ mb: 2, p: 2 }}>
+            <Grid container justifyContent="space-between" alignItems="center">
+              <Grid item>
+                <Typography sx={{ fontSize: '2rem', fontWeight: 500, mr: 1, mt: 1.75, mb: 0.75 }}>All Client</Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" endIcon={<AddIcon />}>
+                  Add New CLient
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Box>
+      </Grid>
+      <Grid item>
+        <ClientsList />
+      </Grid>
+    </Grid>
   );
 }
